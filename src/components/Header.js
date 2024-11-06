15cf6cb2a384dcbe416bf7ecde5e338c72df7b6c
Luan Trindade <luanxpek@gmail.com>
2024-11-06 11:39:53 +0000

    Add the header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Technically Me</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
