7b743e2b2aee0732f7a470a04ff116c3d0447314
Luan Trindade <luanxpek@gmail.com>
2024-11-06 13:28:48 +0000

    New

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h6> technology me</h6>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
