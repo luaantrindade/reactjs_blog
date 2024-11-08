7a75b8c90785b0ebbd685af84f719e891237d392
Luan Trindade <luanxpek@gmail.com>
2024-11-08 17:14:29 +0000

    add background color, aligned header and footer, and some other small changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './styles.css';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
-//import BlogList from './BlogList';
-//import Footer from './Footer';
+import BackgrounColor from './components/BackgroundColor';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
     
 <div className="App">
-    <link rel="stylesheet" href="src/styles.css"></link>
+      <BackgrounColor>
       <Header />  {/* Use the Header component here */}
       <HomePage/>
+      <Footer />
+      </BackgrounColor>
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="fixed inset-x-0 bottom-0 bg-gradient-to-tr p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
+            <div className="flex-auto justify-center md:flex space-x-8 items-center text-black">This is the footer</div>
+        </footer>
+    );
+    
+}
+
+export default Footer;
